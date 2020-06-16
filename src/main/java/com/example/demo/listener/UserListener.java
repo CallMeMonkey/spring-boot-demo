25131d24fc2affc75c9ed6cd8391e92407bfb6fa
CallMeMonkey <hcallmemonkey@gmail.com>
2020-06-16 22:56:23 +0800

    Add redis

--- src/main/java/com/example/demo/listener/UserListener.java
+++ src/main/java/com/example/demo/listener/UserListener.java
@@ -1,13 +1,39 @@
 package com.example.demo.listener;
 
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.omg.PortableServer.LIFESPAN_POLICY_ID;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.security.AllPermission;
+import java.util.List;
 
 @WebListener
 public class UserListener implements ServletContextListener {
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private UserService userService;
+
+    private static final String ALL_USER = "ALL_USER_LIST";
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        //查询数据库中所有用户
+        List<User> userList = userService.findAll();
+        //清楚缓存中的用户数据
+        redisTemplate.delete(ALL_USER);
+        //将数据存放到Redis中
+        redisTemplate.opsForList().leftPushAll(ALL_USER, userList);
+
+        List<User> queryUserList = redisTemplate.opsForList().range(ALL_USER, 0, -1);
+
+        System.out.println("缓存中目前的用户数有 " + queryUserList.size());
         System.out.println("ServletContext上下文初始化");
     }
 
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -5,36 +5,59 @@ import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.transaction.Transactional;
 import java.util.Collection;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
-    @Resource
+    @Resource(name = "userRepository")
     private UserRepository userRepository;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    private static final String ALL_USER = "ALL_USER_LIST";
 
     @Override
     public User findById(String id) {
-        return null;
+        //查询redis中的所有数据
+        List<User> userList = redisTemplate.opsForList().range(ALL_USER, 0, -1);
+        if (userList != null && userList.size() > 0) {
+            for (User user : userList) {
+                if (user.getId().equals(id)) {
+                    return user;
+                }
+            }
+        }
+        //查询数据库中的数据
+        User user = userRepository.findById(id).get();
+        if (user != null) {
+            //将数据插入到redis中
+            redisTemplate.opsForList().leftPush(ALL_USER, user);
+        }
+        return user;
     }
 
     @Override
     public List<User> findAll() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public User save(User user) {
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
     public void delete(String id) {
-
+        userRepository.deleteById(id);
     }
 
     @Override
@@ -44,16 +67,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> findByName(String name) {
-        return null;
+        return userRepository.findByName(name);
     }
 
     @Override
     public List<User> findByNameLike(String name) {
-        return null;
+        return userRepository.findByNameLike(name);
     }
 
     @Override
     public List<User> findByIdIn(Collection<String> ids) {
-        return null;
+        return userRepository.findByIdIn(ids);
     }
 }
